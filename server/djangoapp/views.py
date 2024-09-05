67fdc95cd2db7403030ae5733eb8b50d60f3cca9
Indhira1989 <indharjo@gmail.com>
2024-09-05 18:05:16 -0400

    module4 partial changes

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -109,7 +109,7 @@ def get_dealerships(request, state="All"):
     else:
         endpoint = "/fetchDealers/"+state
     dealerships = get_request(endpoint)
-    return JsonResponse({"status":200,"dealers":dealerships})
+    return JsonResponse({"status":200,"dealers": dealerships})
 
 def get_dealer_reviews(request, dealer_id):
     # if dealer id has been provided
