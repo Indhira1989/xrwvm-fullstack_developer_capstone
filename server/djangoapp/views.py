421a97897276ad96e8d6b664cd93023247b5386d
Indhira1989 <indharjo@gmail.com>
2024-09-06 10:38:18 -0400

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -152,5 +152,5 @@ def add_review(request):
             )
     else:
         return JsonResponse(
-            {"status": 403, "message":"Unauthorized"}
+            {"status": 403, "message": "Unauthorized"}
         )
